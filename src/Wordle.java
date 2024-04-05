051fc154f0c3d5da04220c373a23316f4a62ce51
RB200 <ryanbeaux2@gmail.com>
2024-04-05 10:33:39 -0700

    2

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,6 +1,8 @@
 public class Wordle {
     public static void main(String[] args){
-        System.out.println("asdkjf");
-        System.out.println("Hello there");
+        for(int i = 0; i < 100; i++){
+            int num = (int)(Math.random() * i);
+            System.out.println((int)(Math.random() * i) + " " + i + " " + (i == num));
+        }
     }
 }
