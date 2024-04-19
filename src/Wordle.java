16a2a0e5c2af149487fa087a4b4db6d08abdebd7
RB200 <ryanbeaux2@gmail.com>
2024-04-18 17:43:05 -0700

    updated 5:45pm 4/18 (DONE)

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
 public class Wordle {
 
     static ArrayList<String> allowedWords = new ArrayList<String>();
@@ -14,6 +12,8 @@ public class Wordle {
     static Scanner scan = new Scanner(System.in);
     static char[][] board = new char[6][5];
     static char[][] statusBoard = new char[6][5];
+    static int[] letterCounts = new int[5];
+
     static int row = 0;
 
 
@@ -21,7 +21,7 @@ public class Wordle {
         for(int r = 0; r < board.length; r++){
             for(int c = 0; c < board[r].length; c++){
                 board[r][c] = '■';
-                statusBoard[r][c] = '✕';
+                statusBoard[r][c] = '✖';
 
             }
         }
@@ -40,7 +40,7 @@ public class Wordle {
             System.out.println(e);
         }
         String selectedWord = possibleWords.get((int)(Math.random()*2315));
-        System.out.println(selectedWord);
+
         while(row < 6){
             System.out.print("Input guess: ");
             String playerGuess = scan.next();
@@ -52,40 +52,39 @@ public class Wordle {
                 board[row] = playerGuess.toCharArray();
                 row++;
 
-                System.out.println("BOARD");
-
                 String rowWord = null;
                 int statusBoardRow = 0;
+                System.out.println("  GUESS     STATS");
+                System.out.println("+-------+-----------+");
                 for (char[] word : board) {
                     rowWord = "";
+                    System.out.print("| ");
                     for (char c : word) {
+                        System.out.print(c);
                         rowWord += c;
                     }
-                    System.out.print(rowWord);
-                    System.out.println(Arrays.toString(getStatus(rowWord, selectedWord)));
-
-                    statusBoard[statusBoardRow] = getStatus(rowWord,selectedWord);
-
+                    System.out.print(" | ");
+                    for(char c : getStatus(rowWord,selectedWord)){
+                        System.out.print(c + " ");
+                    }
+                    System.out.print("|");
                     System.out.println();
+                    statusBoard[statusBoardRow] = getStatus(rowWord,selectedWord);
                     statusBoardRow++;
                 }
+                System.out.println("+-------+-----------+");
+
                 if (checkWin(row)) {
                     System.out.println("You win!");
                     break;
                 } else if (row == 6) {
                     System.out.println("You lose. The word was: " + selectedWord.toUpperCase());
                 }
-
-
             }
             else{
                 System.out.println("Invalid word, try again. ");
             }
-
-
         }
-
-        //System.out.println(selectedWord);
     }
     public static boolean isValidWord(String word){
         if(word.length() == 5) {
@@ -101,41 +100,44 @@ public class Wordle {
         char[] pWord = playerWord.toCharArray();
         char[] tWord = targetWord.toCharArray();
         char[] result = new char[5];
+
+        int[] letterApps = new int[26];
+        for(int i = 0; i < pWord.length; i++) {
+            letterApps[(int) (tWord[i]) - 97]++;
+        }
         for(int i = 0; i < pWord.length; i++){
-            if(pWord[i] == tWord[i]){
-                result[i] = '☑';
-            }
-            else{
-                for(int j = 0; j < tWord.length; j++){
-                    if(j != i){
-                        if(pWord[i] == tWord[j]){
-                            result[i] = '~';
+            if((int)pWord[i]-97 < 26){
+                if(letterApps[(int)(pWord[i])-97] > 0){
+                    if(pWord[i] != '■'){
+                        if(pWord[i] == tWord[i]){
+                            result[i] = '*';
                         }
-                        else if(result[i] != '☑'){
-                            result[i] = '✕';
+                        else{
+                            for(int j = 0; j < tWord.length; j++){
+                                if(i != j){
+                                    if(pWord[i] == tWord[j]){
+                                        result[i] = '/';
+                                    }
+                                }
+                            }
                         }
                     }
+                    letterApps[(int)(pWord[i])-97] --;
                 }
             }
         }
         return result;
     }
     public static boolean checkWin(int row){
-
         row--;
         for(int c = 0; c < statusBoard[row].length; c++){
-
-            if(statusBoard[row][c] != '☑') {
-                System.out.println(1);
+            if(statusBoard[row][c] != '*') {
                 return false;
             }
             if(statusBoard[row][0] != statusBoard[row][c]){
-                System.out.println(2);
                 return false;
             }
-
         }
         return true;
-
     }
 }
